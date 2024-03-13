047f1f6b34a48b117361479773959d508452565d
Sergio Daniel Gonzáles López <sdgonzales@conred.gob.gt>
2024-03-13 11:56:34 -0600

    Eliminados archivos del control de versiones según el .gitignore

--- resources/js/app.js
+++
@@ -1,6 +0,0 @@
-/* import swal from 'sweetalert2';
-window.Swal = swal; */
-import './bootstrap';
-import { Datepicker, Input, Modal, Dropdown, Ripple, Datatable, Alert, initTE } from "tw-elements";
-initTE({ Datepicker, Input, Modal, Ripple, Dropdown, Datatable, Alert });
-
