0059e9ae282249eca91ad0e401b8fff1fd053dab
Sergio Daniel Gonzáles López <sdgonzales@conred.gob.gt>
2024-04-17 08:54:16 -0600

    Implementación de dashboard. En proceso...

--- app/Livewire/Dashboard/ContratistasRegionales.php
+++ app/Livewire/Dashboard/ContratistasRegionales.php
@@ -14,8 +14,10 @@ class ContratistasRegionales extends Component
     {
         $this->position = $position;
     }
+
     public function render()
     {
+        
         return view('livewire.dashboard.contratistas-regionales');
     }
 }
