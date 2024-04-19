1573a82ec0a1452d888c18a57ea6e461240d7ae6
Sergio Daniel Gonzáles López <sdgonzales@conred.gob.gt>
2024-04-19 10:34:26 -0600

    Corrección de errores en enrutamiento.

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -603,7 +603,7 @@
                 @else
                     <!-- Logo -->
                     <div class="shrink-0 flex items-center">
-                        <a href="{{ route('dashboard-empleados') }}">
+                        <a href="{{ route('formulario_pir') }}">
                             <x-application-mark class="block h-9 w-auto" />
                         </a>
                     </div>
